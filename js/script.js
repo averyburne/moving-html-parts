0e298642f5c713f2071a22ccfe29dea48dc7227b
Avery Burne <burneav@gmail.com>
2020-09-16 22:25:41 -0400

    added move left and move right buttons
    left will make it go to the opposite side of the screen

--- js/script.js
+++ js/script.js
@@ -10,7 +10,23 @@ function moveDown() {
 function moveUp() {
   const box = document.getElementById('moving-box')
   box.style.position = 'absolute'
-  box.style.left = '100px'
+  box.style.top = '0px'
+  // box.style.right++
+  console.log(box.style)
+}
+
+function moveRight() {
+  const box = document.getElementById('moving-box')
+  box.style.position = 'absolute'
+  box.style.right = '100px'
+  // box.style.right++
+  console.log(box.style)
+}
+
+function moveLeft() {
+  const box = document.getElementById('moving-box')
+  box.style.position = 'absolute'
+  box.style.right = '0px'
   // box.style.right++
   console.log(box.style)
 }
