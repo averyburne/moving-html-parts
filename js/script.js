961adad2c7ce104000ae4f7ee6aeb3f9e3f2785f
Avery Burne <burneav@gmail.com>
2020-09-18 15:06:07 -0400

    Refactored for moving horizontally
    Updated the first function and the event listener to work for moving
    left and right

--- js/script.js
+++ js/script.js
@@ -1,40 +1,28 @@
 const box = $('#moving-box')
 const pane = $('#pane')
 
-const checkHeight = function (maxValue, currentValue, keyPressed) {
-  if (currentValue + 10 >= maxValue && keyPressed === 40) {
+const checkHeight = function (maxValue, currentValue, key) {
+  if (currentValue + 10 >= maxValue && (key === 40 || key === 39 || key === 83 || key === 68)) {
     return parseInt(maxValue, 10) + 'px'
-  } else if (currentValue - 10 <= 10 && keyPressed === 38) {
+  } else if (currentValue - 10 <= 10 && (key === 38 || key === 37 || key === 87 || key === 65)) {
     return '10px'
-  } else if (keyPressed === 40) {
+  } else if (key === 40 || key === 39 || key === 83 || key === 68) {
     return (parseInt(currentValue, 10) + 10 + 'px')
-  } else if (keyPressed === 38) {
+  } else if (key === 38 || key === 37 || key === 87 || key === 65) {
     return (parseInt(currentValue, 10) - 10 + 'px')
   }
 }
 
 document.addEventListener('keydown', function(e) {
   let topValue = parseInt(box.css('top'), 10)
-  let leftValue = box.css('left')
+  let leftValue = parseInt(box.css('left'), 10)
   const width = pane.width() - box.width() + 8
   const height = pane.height() - box.height() + 8
-  console.log(checkHeight(height, topValue, e.which))
-  // moving down with down arrow or S
+  // moving down with up or down with arrows/WS
   if (e.which === 40 || e.which === 38 || e.which === 83 || e.which === 87) {
     box.css('top', checkHeight(height, topValue, e.which))
     // moving left with left arrow or A
-  } else if (e.which === 37 || e.which === 65) {
-    if (parseInt(leftValue, 10) <= 10) {
-      box.css('left', '10px')
-    } else {
-      box.css('left', parseInt(leftValue, 10) - 10 + 'px')
-    }
-    // moving right with right arrow or D
-  } else if (e.which === 39 || e.which === 68) {
-    if (parseInt(leftValue, 10) >= width) {
-      box.css('left', width + 'px')
-    } else {
-      box.css('left', parseInt(leftValue, 10) + 10 + 'px')
-    }
+  } else if (e.which === 39 || e.which === 37 || e.which === 68 || e.which === 65) {
+    box.css('left', checkHeight(width, leftValue, e.which))
   }
 })
