1238ff64e1fb403daabba758ae66cc6b03d58412
Avery Burne <burneav@gmail.com>
2020-09-23 15:17:22 -0400

    added second box that can also move ball around
    both boxes currently use the same controls to move and therefore move simultaneously

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const box = $('#moving-box')
+const box2 = $('#moving-box2')
 const pane = $('#pane')
 const ball = $('#ball')
 
@@ -27,6 +28,30 @@ document.addEventListener('keydown', function(e) {
     // moving left with left arrow or A
   } else if (e.which === 39 || e.which === 37 || e.which === 68 || e.which === 65) {
     box.css('left', moveBox(width, leftValue, e.which))
+  }
+  if ((leftValueBall <= (leftValue + 80) && leftValueBall >= (leftValue - 40)) && (topValueBall <= (topValue + 60) && topValueBall >= (topValue - 20)) && (e.which === 68 || e.which === 39)) {
+    ball.css('left', (parseInt(leftValueBall, 10) + 50 + 'px'))
+  } else if ((leftValueBall >= (leftValue - 40) && leftValueBall <= (leftValue + 80)) && (topValueBall <= (topValue + 60) && topValueBall >= (topValue - 20)) && (e.which === 65 || e.which === 37)) {
+    ball.css('left', parseInt(leftValueBall, 10) - 50 + 'px')
+  } else if ((topValueBall <= (topValue + 80) && topValueBall >= (topValue - 40)) && (leftValueBall <= (leftValue + 60)) && leftValueBall >= (leftValue - 20) && (e.which === 83 || e.which === 40)) {
+    ball.css('top', parseInt(topValueBall, 10) + 50 + 'px')
+  } else if ((topValueBall >= (topValue - 40) && topValueBall <= (topValue + 80)) && (leftValueBall <= (leftValue + 60)) && leftValueBall >= (leftValue - 20) && (e.which === 87 || e.which === 38)) {
+    ball.css('top', parseInt(topValueBall, 10) - 50 + 'px')
+  }
+})
+
+document.addEventListener('keydown', function(e) {
+  let topValue = parseInt(box2.css('top'), 10)
+  let leftValue = parseInt(box2.css('left'), 10)
+  let topValueBall = parseInt(ball.css('top'), 10)
+  let leftValueBall = parseInt(ball.css('left'), 10)
+  const width = pane.width() - box2.width() + 8
+  const height = pane.height() - box2.height() + 8
+  if (e.which === 40 || e.which === 38 || e.which === 83 || e.which === 87) {
+    box2.css('top', moveBox(height, topValue, e.which))
+    // moving left with left arrow or A
+  } else if (e.which === 39 || e.which === 37 || e.which === 68 || e.which === 65) {
+    box2.css('left', moveBox(width, leftValue, e.which))
   }
   if ((leftValueBall <= (leftValue + 80) && leftValueBall >= (leftValue - 40)) && (topValueBall <= (topValue + 60) && topValueBall >= (topValue - 20)) && (e.which === 68 || e.which === 39)) {
     ball.css('left', (parseInt(leftValueBall, 10) + 50 + 'px'))
