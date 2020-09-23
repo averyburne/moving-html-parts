be539cf9cf4e9c97b9edb0481aed5f2c87e2ecc9
Avery Burne <burneav@gmail.com>
2020-09-23 15:48:41 -0400

    Changed controls for both boxes
    the first box uses WASD and the second box uses arrow keys
    fixed bug where one box could move the ball even if it did not meet the
    correct range (vertical or horizontal) because it was in the correct
    range of the other box

--- js/script.js
+++ js/script.js
@@ -23,19 +23,19 @@ document.addEventListener('keydown', function(e) {
   const width = pane.width() - box.width() + 8
   const height = pane.height() - box.height() + 8
   // moving down with up or down with arrows/WS
-  if (e.which === 40 || e.which === 38 || e.which === 83 || e.which === 87) {
+  if (e.which === 83 || e.which === 87) {
     box.css('top', moveBox(height, topValue, e.which))
     // moving left with left arrow or A
-  } else if (e.which === 39 || e.which === 37 || e.which === 68 || e.which === 65) {
+  } else if (e.which === 68 || e.which === 65) {
     box.css('left', moveBox(width, leftValue, e.which))
   }
-  if ((leftValueBall <= (leftValue + 80) && leftValueBall >= (leftValue - 40)) && (topValueBall <= (topValue + 60) && topValueBall >= (topValue - 20)) && (e.which === 68 || e.which === 39)) {
+  if ((leftValueBall <= (leftValue + 80) && leftValueBall >= (leftValue - 40)) && (topValueBall <= (topValue + 60) && topValueBall >= (topValue - 20)) && e.which === 68) {
     ball.css('left', (parseInt(leftValueBall, 10) + 50 + 'px'))
-  } else if ((leftValueBall >= (leftValue - 40) && leftValueBall <= (leftValue + 80)) && (topValueBall <= (topValue + 60) && topValueBall >= (topValue - 20)) && (e.which === 65 || e.which === 37)) {
+  } else if ((leftValueBall >= (leftValue - 40) && leftValueBall <= (leftValue + 80)) && (topValueBall <= (topValue + 60) && topValueBall >= (topValue - 20)) && e.which === 65) {
     ball.css('left', parseInt(leftValueBall, 10) - 50 + 'px')
-  } else if ((topValueBall <= (topValue + 80) && topValueBall >= (topValue - 40)) && (leftValueBall <= (leftValue + 60)) && leftValueBall >= (leftValue - 20) && (e.which === 83 || e.which === 40)) {
+  } else if ((topValueBall <= (topValue + 80) && topValueBall >= (topValue - 40)) && (leftValueBall <= (leftValue + 60)) && leftValueBall >= (leftValue - 20) && e.which === 83) {
     ball.css('top', parseInt(topValueBall, 10) + 50 + 'px')
-  } else if ((topValueBall >= (topValue - 40) && topValueBall <= (topValue + 80)) && (leftValueBall <= (leftValue + 60)) && leftValueBall >= (leftValue - 20) && (e.which === 87 || e.which === 38)) {
+  } else if ((topValueBall >= (topValue - 40) && topValueBall <= (topValue + 80)) && (leftValueBall <= (leftValue + 60)) && leftValueBall >= (leftValue - 20) && e.which === 87) {
     ball.css('top', parseInt(topValueBall, 10) - 50 + 'px')
   }
 })
@@ -47,19 +47,21 @@ document.addEventListener('keydown', function(e) {
   let leftValueBall = parseInt(ball.css('left'), 10)
   const width = pane.width() - box2.width() + 8
   const height = pane.height() - box2.height() + 8
-  if (e.which === 40 || e.which === 38 || e.which === 83 || e.which === 87) {
+  console.log(ball.css('left'))
+  console.log(box.css('left'))
+  if (e.which === 40 || e.which === 38) {
     box2.css('top', moveBox(height, topValue, e.which))
     // moving left with left arrow or A
-  } else if (e.which === 39 || e.which === 37 || e.which === 68 || e.which === 65) {
+  } else if (e.which === 39 || e.which === 37) {
     box2.css('left', moveBox(width, leftValue, e.which))
   }
-  if ((leftValueBall <= (leftValue + 80) && leftValueBall >= (leftValue - 40)) && (topValueBall <= (topValue + 60) && topValueBall >= (topValue - 20)) && (e.which === 68 || e.which === 39)) {
+  if ((leftValueBall <= (leftValue + 80) && leftValueBall >= (leftValue - 40)) && (topValueBall <= (topValue + 60) && topValueBall >= (topValue - 20)) && e.which === 39) {
     ball.css('left', (parseInt(leftValueBall, 10) + 50 + 'px'))
-  } else if ((leftValueBall >= (leftValue - 40) && leftValueBall <= (leftValue + 80)) && (topValueBall <= (topValue + 60) && topValueBall >= (topValue - 20)) && (e.which === 65 || e.which === 37)) {
+  } else if ((leftValueBall >= (leftValue - 40) && leftValueBall <= (leftValue + 80)) && (topValueBall <= (topValue + 60) && topValueBall >= (topValue - 20)) && e.which === 37) {
     ball.css('left', parseInt(leftValueBall, 10) - 50 + 'px')
-  } else if ((topValueBall <= (topValue + 80) && topValueBall >= (topValue - 40)) && (leftValueBall <= (leftValue + 60)) && leftValueBall >= (leftValue - 20) && (e.which === 83 || e.which === 40)) {
+  } else if ((topValueBall <= (topValue + 80) && topValueBall >= (topValue - 40)) && (leftValueBall <= (leftValue + 60)) && leftValueBall >= (leftValue - 20) && e.which === 40) {
     ball.css('top', parseInt(topValueBall, 10) + 50 + 'px')
-  } else if ((topValueBall >= (topValue - 40) && topValueBall <= (topValue + 80)) && (leftValueBall <= (leftValue + 60)) && leftValueBall >= (leftValue - 20) && (e.which === 87 || e.which === 38)) {
+  } else if ((topValueBall >= (topValue - 40) && topValueBall <= (topValue + 80)) && (leftValueBall <= (leftValue + 60)) && leftValueBall >= (leftValue - 20) && e.which === 38) {
     ball.css('top', parseInt(topValueBall, 10) - 50 + 'px')
   }
 })
